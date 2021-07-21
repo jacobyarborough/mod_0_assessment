1cff2ba3c6b1a0f2d4a9119552722e8d0c5e663f
Jacob Yarborough <jayarborough95@gmail.com>
2021-07-21 12:13:51 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+famous_person = "Lebron James"
+is_active = true
+famous_person = "Michael Jordan"
+num_3 = 23
+num_9 = 24
+sum = num_3 + num_9
+animals = ["Lion", "Tiger", "Wolf"]
+animals.shift
+print famous_person.length
