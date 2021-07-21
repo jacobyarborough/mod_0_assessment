f1912d2e2d3b9233b655f6b2c0fb2e07e48cf76d
Jacob Yarborough <jayarborough95@gmail.com>
2021-07-21 12:24:27 -0600

    Add school hash model

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+school_details = {
+  school_name: "North High School",
+  school_district: "Denver School District",
+  num_of_students: 2000,
+  num_of_staff: 120.00,
+  is_in_session: false,
+  has_soccer_team: true,
+  rival_schools: ["West High School", "East High School", "South High School"],
+  grades_taught: [9, 10, 11, 12]
+}
