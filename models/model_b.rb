eb0f84bdb1e52a9e87a0e676f5c39364a0754a5e
Jacob Yarborough <jayarborough95@gmail.com>
2021-07-21 12:29:32 -0600

    Add pet_profile hash model

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+pet_profile = {
+  pet_type: "Dog",
+  pet_name: "Spot",
+  pet_age: 5,
+  pet_weight: 75.5,
+  is_playing: true,
+  is_sleeping: false,
+  pet_colors: ["Black", "White", "Brown"],
+  pet_playmates_names: ["Lassy", "Fido", "Shadow"]
+}
